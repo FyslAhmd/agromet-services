ceddf4fd7ce0bab7657d62bfae64b63f85844ba1
FyslAhmd <info.faysal.32@gmail.com>
2026-02-03 13:12:07 +0600

    prod

--- src/app.js
+++ src/app.js
@@ -44,7 +44,23 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: join(__dirname, '..', '.env') });
 
 const app = express();
-app.use(cors());
+
+// CORS configuration
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:3000',
+    'https://agromet.brri.gov.bd',
+    'https://saads.brri.gov.bd',
+    'https://ccms.brri.gov.bd',
+    'https://dcrs.brri.gov.bd'
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
